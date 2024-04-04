f92a0adc07477b0b1a7c2d66af3887bbd57aa058
Felix Narciso <49419433+felixnarciso@users.noreply.github.com>
2024-04-04 02:08:41 +0000

    Tweaks and adding more functionality

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ document.querySelector(`.guess`).value = 23;
 console.log(document.querySelector(`.guess`).value);
 */
 
-const correctNumber = Math.trunc(Math.random()*20) + 1;
+let correctNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20;
-document.querySelector(`.number`).textContent = correctNumber;
+
 
 document.querySelector(`.check`).addEventListener(`click`, function(){
 	const guess = Number(document.querySelector(`.guess`).value);
@@ -24,6 +24,7 @@ document.querySelector(`.check`).addEventListener(`click`, function(){
 		document.querySelector(`.message`).textContent = `🚫 No number!`;
 	}else if (guess === correctNumber) //when player wins
 	{
+		document.querySelector(`.number`).textContent = correctNumber;
 		document.querySelector(`.message`).textContent = `Correct Number! :D`;
 		document.querySelector(`body`).style.backgroundColor = `#60b347`;
 		document.querySelector(`.number`).style.width = `30rem`;
@@ -54,3 +55,14 @@ document.querySelector(`.check`).addEventListener(`click`, function(){
 		}
 	}
 });
+
+document.querySelector(`.again`).addEventListener(`click`, function(){
+	document.querySelector(`body`).style.backgroundColor = `#222`;
+	document.querySelector(`.message`).textContent = `Start guessing...`;
+	score = 20;
+	document.querySelector(`.score`).textContent = score;
+	document.querySelector(`.number`).style.width = `15rem`;
+	document.querySelector(`.guess`).value = ``;
+	document.querySelector(`.number`).textContent = `?`;
+	correctNumber = Math.trunc(Math.random()*20) + 1;
+})
