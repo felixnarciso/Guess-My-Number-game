c013155285fc88617b9303bad6ac02af458b8288
Felix Narciso <49419433+felixnarciso@users.noreply.github.com>
2024-04-04 16:16:02 +0000

    Refactoring code

--- script.js
+++ script.js
@@ -14,43 +14,42 @@ console.log(document.querySelector(`.guess`).value);
 
 let correctNumber = Math.trunc(Math.random()*20) + 1;
 let score = 20;
+let highScore = 0;
 
+const displayMessage = function(message)
+{
+	document.querySelector(`.message`).textContent = message;
+}
 
 document.querySelector(`.check`).addEventListener(`click`, function(){
 	const guess = Number(document.querySelector(`.guess`).value);
 
 	if (!guess) //when there is no input
 	{
-		document.querySelector(`.message`).textContent = `🚫 No number!`;
+		// document.querySelector(`.message`).textContent = `🚫 No number!`; Same thing as
+		displayMessage(`🚫 No number!`);
 	}else if (guess === correctNumber) //when player wins
 	{
 		document.querySelector(`.number`).textContent = correctNumber;
-		document.querySelector(`.message`).textContent = `Correct Number! :D`;
+		displayMessage(`Correct Number! :D`);
 		document.querySelector(`body`).style.backgroundColor = `#60b347`;
 		document.querySelector(`.number`).style.width = `30rem`;
-	}else if (guess > correctNumber) //when guess is too high
-	{
-		if (score > 1)
+
+		if (score > highScore)
 		{
-			score--;
-			document.querySelector(`.score`).textContent = score;
-			document.querySelector(`.message`).textContent = `📈 Too high!`;
-		}else
-		{
-			document.querySelector(`.message`).textContent = `💥 You lost the game!`
-			document.querySelector(`.score`).textContent = 0;
+			highScore = score;
+			document.querySelector(`.highscore`).textContent = highScore;
 		}
-		
-	}else if (guess < correctNumber) //when guess is too low
+	}else if (guess !== correctNumber)
 	{
 		if (score > 1)
 		{
 			score--;
 			document.querySelector(`.score`).textContent = score;
-			document.querySelector(`.message`).textContent = `📉 Too low!`;
+			displayMessage(guess > correctNumber ? `📈 Too high!` : `📉 Too low!`);
 		}else
 		{
-			document.querySelector(`.message`).textContent = `💥 You lost the game!`
+			displayMessage(`💥 You lost the game!`);
 			document.querySelector(`.score`).textContent = 0;
 		}
 	}
@@ -58,7 +57,7 @@ document.querySelector(`.check`).addEventListener(`click`, function(){
 
 document.querySelector(`.again`).addEventListener(`click`, function(){
 	document.querySelector(`body`).style.backgroundColor = `#222`;
-	document.querySelector(`.message`).textContent = `Start guessing...`;
+		displayMessage(`Start guessing...`);
 	score = 20;
 	document.querySelector(`.score`).textContent = score;
 	document.querySelector(`.number`).style.width = `15rem`;
